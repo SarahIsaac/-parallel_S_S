552d0bcb10c29a3da73ad31ed189929fa60207cc
SarahIsaac <sarahisaac31@gmail.com>
2016-10-01 20:48:48 -0600

    move to nanoseconds

--- Timer.h
+++ Timer.h
@@ -6,6 +6,6 @@ double functionTimer(F f)
 	std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
 	f();
 	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-	std::chrono::microseconds result = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+	std::chrono::nanoseconds result = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
 	return result.count();
 }
